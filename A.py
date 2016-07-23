84ebb402221a48495e655024098743eed075236f
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-07-24 00:33:55 +0300

    A: Do binary search list as set is in Lesson 10

--- A.py
+++ A.py
@@ -16,12 +16,30 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+def BinInsCheck(l, v):
+    b = 0
+    e = len(l)
+    while True:
+        m = b + (e - b) // 2
+        if e == b:
+            l.insert(e, v)
+            return False
+        if v == l[m]:
+            return True
+
+        if v > l[m]:
+            b = m + 1
+        if v < l[m]:
+            e = m
+
 def Repeated(s):
     l = list(map(int, s.split()))
+    s = []
+
     for v in l:
-        if l.count(v) == 2:
+        if BinInsCheck(s, v):
             return v
-    return -1
+    return None
 
 fi = open('input.txt', 'r')
 fo = open('output.txt', 'w')
